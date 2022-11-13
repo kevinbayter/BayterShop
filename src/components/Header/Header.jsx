6596d9981457587fbdc53a58fd17b3fd4032bf46
Kevin Bayter <kevin.kbayter@mercadolibre.com.co>
2022-11-12 23:37:51 -0500

    Frontend finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useRef, useEffect } from "react";
 
 import { NavLink } from 'react-router-dom';
 import "./header.css";
@@ -26,8 +26,31 @@ const nav__links = [
 ]
 
 const Header = () => {
+
+  const headerRef = useRef(null);
+
+  const menuRef = useRef(null);
+
+  const stickyHeaderFunc = () => {
+    window.addEventListener('scroll', () => {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add('sticky__header')
+      } else {
+        headerRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+
+  useEffect(() => {
+    stickyHeaderFunc();
+
+    return () => window.removeEventListener('scroll', stickyHeaderFunc)
+  });
+
+  const menuToggle = () => menuRef.current.classList.toggle('active__menu');
+
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <Container>
         <Row>
           <div className="nav__wrapper">
@@ -38,7 +61,7 @@ const Header = () => {
               </div>
             </div>
 
-            <div className="navigation">
+            <div className="navigation" ref={menuRef} onClick={menuToggle}>
               <ul className="menu">
                 {nav__links.map((item, index) => (
                   <li className="nav__item" key={index}>
@@ -73,11 +96,9 @@ const Header = () => {
                   alt="user icon"
                 />{" "}
               </span>
-            </div>
-            <div clasName="mobile__menu">
-              <span>
-                <i class="ri-menu-line"></i>
-              </span>
+              <div clasName="mobile__menu">
+                <span onClick={menuToggle}><i class="ri-menu-line"></i></span>
+              </div>
             </div>
           </div>
         </Row>
