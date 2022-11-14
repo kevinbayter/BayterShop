1ec7206218b34f5c1895d8488c354f2dd1cf3cc2
Kevin Bayter <kevin.kbayter@mercadolibre.com.co>
2022-11-13 23:41:29 -0500

    Frontend sucessfuly

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React,{ useRef, useEffect } from "react";
 
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import "./header.css";
 
 import { motion } from 'framer-motion';
@@ -9,6 +9,7 @@ import logo from "../../assets/images/eco-logo.png";
 import userIcon from "../../assets/images/user-icon.png";
 
 import { Container, Row } from "reactstrap";
+import { useSelector } from "react-redux";
 
 const nav__links = [
   {
@@ -28,8 +29,10 @@ const nav__links = [
 const Header = () => {
 
   const headerRef = useRef(null);
+  const totalQuantity = useSelector(state => state.cart.totalQuantity)
 
   const menuRef = useRef(null);
+  const navigate = useNavigate();
 
   const stickyHeaderFunc = () => {
     window.addEventListener('scroll', () => {
@@ -48,6 +51,10 @@ const Header = () => {
   });
 
   const menuToggle = () => menuRef.current.classList.toggle('active__menu');
+
+  const navigateToCart = () => {
+    navigate('/cart')
+  }
 
   return (
     <header className="header" ref={headerRef}>
@@ -83,10 +90,10 @@ const Header = () => {
                 <i class="ri-heart-line"></i>
                 <span className="badge">1</span>
               </span>
-              <span className="cart__icon">
+              <span className="cart__icon" onClick={navigateToCart}>
                 {" "}
                 <i class="ri-shopping-bag-line"></i>
-                <span className="badge">1</span>
+                <span className="badge">{ totalQuantity }</span>
               </span>
               <span>
                 {" "}
--- src/components/UI/ProductCard.jsx
+++ src/components/UI/ProductCard.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-import '../../styles/product-cad.css'
-import { Col } from 'reactstrap'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { motion } from "framer-motion";
+import "../../styles/product-cad.css";
+import { Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 
-const ProductCard = ({item}) => {
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../redux/slices/cartSlice";
+
+const ProductCard = ({ item }) => {
+  const dispatch = useDispatch();
+  const addToCard = () => {
+    dispatch(
+      cartActions.addItem({
+        id: item.id,
+        productName: item.productName,
+        price: item.price,
+        imgUrl: item.imgUrl,
+      })
+    );
+
+    toast.success("Product added successfully");
+  };
   return (
     <Col lg="3" md="4" className="mb-2">
       <div className="product__item">
@@ -17,19 +34,19 @@ const ProductCard = ({item}) => {
         </div>
         <div className="p-2" product__info>
           <h3 className="product__name">
-                      <Link to={ `/shop/${item.id}` }>{ item.productName }</Link>
+            <Link to={`/shop/${item.id}`}>{item.productName}</Link>
           </h3>
-                  <span>{ item.category }</span>
+          <span>{item.category}</span>
         </div>
         <div className="product__card-bottom d-flex align-items-center justify-content-between p-2">
-                  <span className="price">{ item.price }</span>
-          <motion.span whileTap={{ scale: 1.2 }}>
+          <span className="price">{item.price}</span>
+          <motion.span whileTap={{ scale: 1.2 }} onClick={addToCard}>
             <i class="ri-add-line"></i>
           </motion.span>
         </div>
       </div>
     </Col>
   );
-}
+};
 
-export default ProductCard
+export default ProductCard;
